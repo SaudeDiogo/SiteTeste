ba97602039baa49ebcea7f0c3996ddf1ce3d59a8
Diogo Saúde <diogosaude0@gmail.com>
2018-09-25 03:24:02 -0300

    testes 2

--- geral.js
+++ geral.js
@@ -1,10 +1,18 @@
 //************************
 // inserindo modelo-DS
 //************************
-    var css = document.createElement("link");
-    css.setAttribute("rel", "stylesheet");
-    css.setAttribute("type", "text/css");
-    css.setAttribute("href", "css/estilo.css");
-    document.getElementsByTagName("head")[0].appendChild(css);
+var css = document.createElement("link");
+css.setAttribute("rel", "stylesheet");
+css.setAttribute("type", "text/css");
+css.setAttribute("href", "css/estilo.css");
+document.getElementsByTagName("head")[0].appendChild(css);
 
-    document.body.classList.add("modelo-DS");
+document.body.classList.add("modelo-DS");
+
+var text = '{ "inicial" : [' +
+'{ "0":"Aulas"},' +
+'{ "segundoCartao":"2"},' +
+'{ "terceiroCartao":"3"},' +
+'{ "quartoCartao":"4"} ]}';
+    
+document.getElementsByClassName(".cartao.primeiro").innerHTML = JSON.parse(text).inicial[0];
