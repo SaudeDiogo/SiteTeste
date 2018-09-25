3dce35ecdbef4b2a2804d1dd16e6380d778657ea
Diogo Saúde <diogosaude0@gmail.com>
2018-09-25 02:47:28 -0300

    teste
    testando

---
+++ geral.js
@@ -0,0 +1,10 @@
+//************************
+// inserindo modelo-DS
+//************************
+    var css = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("href", "css/estilo.css");
+    document.getElementsByTagName("head")[0].appendChild(css);
+
+    document.body.classList.add("modelo-DS");
